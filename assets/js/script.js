db3a73b91d095c5a7ad5d77c9238b18ff2846138
chigichan24 <kazuki.chigichan@gmail.com>
2017-06-10 01:26:35 +0900

    Add const value

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,19 @@ function init() {
     beef.y = (480 - 230) / 2;
     stage.update();
 }
+
+const Neck = 0;
+const Shoulder = 1;
+const Tombi = 2;
+const Misuji = 3;
+const ShoulderLoin = 4;
+const RibLoin = 5;
+const Sirloin = 6;
+const Fillet = 7;
+const Bara = 8;
+const Uchimomo = 9;
+const Shintama =10;
+const Sotomomo = 11;
+const Lampu = 12;
+const Ichibo = 13;
+const Sune = 14;
